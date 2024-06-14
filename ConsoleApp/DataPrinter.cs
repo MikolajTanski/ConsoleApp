18380233a86f7bd93dabd7d52de50d0f4a887680
Mikolaj Tanski <mikolajtanski1@gmail.com>
2024-06-14 18:35:25 +0200

    Extract Printer functionality
    - Extracted printing functionality into a separate Printer.cs file.
    - Updated Parser.cs to utilize the new Printer class.
    - This change improves code modularity and separation of concerns, making the printing process more maintainable and testable.

---
+++ ConsoleApp/DataPrinter.cs
@@ -0,0 +1,75 @@
+﻿namespace ConsoleApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DataPrinter
+    {
+        public void Print(IList<DataSourceObject> dataSource)
+        {
+            foreach (var dataSourceObject in dataSource.OrderBy(x => x.Type))
+            {
+                switch (dataSourceObject.Type)
+                {
+                    case "DATABASE":
+                    case "GLOSSARY":
+                    case "DOMAIN":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"{dataSourceObject.Type} '{dataSourceObject.Name} ({dataSourceObject.Title})'");
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine(dataSourceObject.Description);
+                        Console.ResetColor();
+
+                        // direct children of database like tables, procedures, lookups
+                        var childrenGroups = dataSource
+                            .Where(x =>
+                                x.ParentId == dataSourceObject.Id &&
+                                x.ParentType == dataSourceObject.Type)
+                            .GroupBy(x => x.Type);
+
+                        foreach (var childrenGroup in childrenGroups)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
+                            Console.WriteLine($"\t{childrenGroup.Key}S ({childrenGroup.Count()}):");
+                            Console.ResetColor();
+
+                            foreach (var child in childrenGroup.OrderBy(x => x.Name))
+                            {
+                                // direct sub children like columns, parameters, values
+                                var subChildrenGroups = dataSource
+                                    .Where(x =>
+                                        x.ParentId == child.Id &&
+                                        x.ParentType == child.Type)
+                                    .GroupBy(x => x.Type);
+
+                                Console.WriteLine($"\t\t{child.Schema}.{child.Name} ({child.Title})");
+                                Console.ForegroundColor = ConsoleColor.DarkGray;
+                                Console.WriteLine($"\t\t{child.Description}");
+                                Console.ResetColor();
+
+                                foreach (var subChildrenGroup in subChildrenGroups)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                                    Console.WriteLine($"\t\t\t{subChildrenGroup.Key}S ({subChildrenGroup.Count()}):");
+                                    Console.ResetColor();
+
+                                    foreach (var subChild in subChildrenGroup.OrderBy(x => x.Name))
+                                    {
+                                        Console.WriteLine($"\t\t\t\t{subChild.Name} ({subChild.Title})");
+                                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                                        Console.WriteLine($"\t\t\t\t{subChild.Description}");
+                                        Console.ResetColor();
+                                    }
+                                }
+                            }
+                        }
+
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- ConsoleApp/Parser.cs
+++ ConsoleApp/Parser.cs
@@ -5,16 +5,19 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Reflection;
 
     public class Parser
     {
+        private readonly DataPrinter _dataPrinter;
         private readonly DataLoader _dataLoader;
         private IList<ImportedObject> ImportedObjects;
         private IList<DataSourceObject> DataSource;
         
-        public Parser(DataLoader dataLoader)
+        public Parser(DataLoader dataLoader, DataPrinter dataPrinter)
         {
             _dataLoader = dataLoader;
+            _dataPrinter = dataPrinter;
         }
 
         public void Do(string fileToImport, string dataSource)
@@ -22,73 +25,7 @@
             ImportedObjects = _dataLoader.Import(fileToImport);
             DataSource = _dataLoader.Load(dataSource);
             MatchAndUpdate();
-            Print();
-        }
-
-        private void Print()
-        {
-            foreach (var dataSourceObject in this.DataSource.OrderBy(x => x.Type))
-            {
-                switch (dataSourceObject.Type)
-                {
-                    case "DATABASE":
-                    case "GLOSSARY":
-                    case "DOMAIN":
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine($"{dataSourceObject.Type} '{dataSourceObject.Name} ({dataSourceObject.Title})'");
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.WriteLine(dataSourceObject.Description);
-                        Console.ResetColor();
-
-                        // direct children of database like tables, procedures, lookups
-                        var childrenGroups = this.DataSource
-                            .Where(x =>
-                                x.ParentId == dataSourceObject.Id &&
-                                x.ParentType == dataSourceObject.Type)
-                            .GroupBy(x => x.Type);
-
-                        foreach (var childrenGroup in childrenGroups)
-                        {
-                            Console.ForegroundColor = ConsoleColor.DarkCyan;
-                            Console.WriteLine($"\t{childrenGroup.Key}S ({childrenGroup.Count()}):");
-                            Console.ResetColor();
-
-                            foreach (var child in childrenGroup.OrderBy(x => x.Name))
-                            {
-                                // direct sub children like columns, parameters, values
-                                var subChildrenGroups = this.DataSource
-                                    .Where(x =>
-                                        x.ParentId == child.Id &&
-                                        x.ParentType == child.Type)
-                                    .GroupBy(x => x.Type);
-
-                                Console.WriteLine($"\t\t{child.Schema}.{child.Name} ({child.Title})");
-                                Console.ForegroundColor = ConsoleColor.DarkGray;
-                                Console.WriteLine($"\t\t{child.Description}");
-                                Console.ResetColor();
-
-                                foreach (var subChildrenGroup in subChildrenGroups)
-                                {
-                                    Console.ForegroundColor = ConsoleColor.DarkCyan;
-                                    Console.WriteLine($"\t\t\t{subChildrenGroup.Key}S ({subChildrenGroup.Count()}):");
-                                    Console.ResetColor();
-
-                                    foreach (var subChild in subChildrenGroup.OrderBy(x => x.Name))
-                                    {
-                                        Console.WriteLine($"\t\t\t\t{subChild.Name} ({subChild.Title})");
-                                        Console.ForegroundColor = ConsoleColor.DarkGray;
-                                        Console.WriteLine($"\t\t\t\t{subChild.Description}");
-                                        Console.ResetColor();
-                                    }
-                                }
-                            }
-                        }
-
-                        break;
-                }
-            }
-
-            Console.ReadKey();
+            _dataPrinter.Print(DataSource);
         }
 
         private void MatchAndUpdate()
