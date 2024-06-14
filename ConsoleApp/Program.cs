97597b0802421f93e6ada262434984fed50354ab
Mikolaj Tanski <mikolajtanski1@gmail.com>
2024-06-14 17:38:06 +0200

    Init commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var reader = new Parser();
+            //reader.Do("sampleFile1.csv", "dataSource.csv");
+            //reader.Do("sampleFile2.csv", "dataSource.csv");
+            reader.Do("sampleFile3.csv", "dataSource.csv");
+        }
+    }
+}
