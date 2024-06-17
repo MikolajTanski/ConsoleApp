428715e352b86f8846fd0c8d39acb9c392746a4e
Mikolaj Tanski <mikolajtanski1@gmail.com>
2024-06-17 08:48:26 +0200

    chenges in program cs for logger

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,47 +1,48 @@
-﻿using System;
+﻿using ConsoleApp;
+using ConsoleApp.Helpers;
+using System;
 using System.Threading.Tasks;
 
-namespace ConsoleApp
+internal class Program
 {
-    internal class Program
+    private static async Task Main()
     {
-        private static async Task Main()
+        string logFilePath = ":) patch";
+        var logHelper = new LogHelper(logFilePath);
+        var dataLoader = new DataLoader(logHelper);
+        var dataPrinter = new DataPrinter();
+        var dataMatcher = new DataMatcher(logHelper);
+        var parser = new Parser(dataLoader, dataPrinter, dataMatcher);
+
+        Console.WriteLine("Choose a file to import:");
+        Console.WriteLine("1 - sampleFile1.csv");
+        Console.WriteLine("2 - sampleFile2.csv");
+        Console.WriteLine("3 - sampleFile3.csv");
+
+        var key = Console.ReadKey();
+        Console.WriteLine();
+
+        string fileToImport = null;
+
+        switch (key.Key)
         {
-            var dataLoader = new DataLoader();
-            var dataPrinter = new DataPrinter();
-            var dataMatcher = new DataMatcher();
-            var parser = new Parser(dataLoader, dataPrinter, dataMatcher);
+            case ConsoleKey.D1:
+            case ConsoleKey.NumPad1:
+                fileToImport = "Data\\sampleFile1.csv";
+                break;
+            case ConsoleKey.D2:
+            case ConsoleKey.NumPad2:
+                fileToImport = "Data\\sampleFile2.csv";
+                break;
+            case ConsoleKey.D3:
+            case ConsoleKey.NumPad3:
+                fileToImport = "Data\\sampleFile3.csv";
+                break;
+        }
 
-            Console.WriteLine("Choose a file to import:");
-            Console.WriteLine("1 - sampleFile1.csv");
-            Console.WriteLine("2 - sampleFile2.csv");
-            Console.WriteLine("3 - sampleFile3.csv");
-
-            var key = Console.ReadKey();
-            Console.WriteLine();
-
-            string fileToImport = null;
-
-            switch (key.Key)
-            {
-                case ConsoleKey.D1:
-                case ConsoleKey.NumPad1:
-                    fileToImport = "Data\\sampleFile1.csv";
-                    break;
-                case ConsoleKey.D2:
-                case ConsoleKey.NumPad2:
-                    fileToImport = "Data\\sampleFile2.csv";
-                    break;
-                case ConsoleKey.D3:
-                case ConsoleKey.NumPad3:
-                    fileToImport = "Data\\sampleFile3.csv";
-                    break;
-            }
-
-            if (fileToImport != null)
-            {
-                await parser.ProcessDataAsync(fileToImport, "Data\\dataSource.csv");
-            }
+        if (fileToImport != null)
+        {
+            await parser.ProcessDataAsync(fileToImport, "Data\\dataSource.csv");
         }
     }
 }
